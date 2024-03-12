bf69535a665a1fd731e54a1054fd0a66d4c571f2
Ryzhkova <Tatyana.Va.Ryzhkova@rosbank.ru>
2024-03-12 15:36:02 +0300

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,12 @@ import picocli.CommandLine;
         mixinStandardHelpOptions = true
 )
 public class App implements Runnable{
+    @CommandLine.Parameters(index = "0", paramLabel="filepath1", description = "path to first file")
+    private String filepath1;
+    @CommandLine.Parameters(index = "1", paramLabel="filepath2", description = "path to first file")
+    private String filepath2;
+    @CommandLine.Option(names = {"-f, --format"}, paramLabel="format", description = "output format [default: stylish]")
+    private String format = "stylish";
     public static void main(String[] args){
         new CommandLine(new App()).execute(args);
     }
