81d479c61198ab9d50d14c38dbf7d3e7b068f4b6
Ryzhkova <Tatyana.Va.Ryzhkova@rosbank.ru>
2024-03-12 15:20:34 +0300

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true
+)
+public class App implements Runnable{
+    public static void main(String[] args){
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello");
     }
 }
