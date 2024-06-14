386a4bc67116226757c35a098eed3ea910b474eb
Ryzhkova <Tatyana.Va.Ryzhkova@rosbank.ru>
2024-06-14 20:35:34 +0300

    tetsss

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -1,11 +1,16 @@
 package hexlet.code.formatters;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Stylish implements Format {
 
     @Override
     public String generateEquals(Map<String, Object> map) {
+        Map<String, String> mapString = new HashMap<>();
+        for (Map.Entry<String, Object> m : map.entrySet()) {
+            mapString.put(m.getKey(), m.getValue() == null ? null : m.getValue().toString());
+        }
         String s = map.toString()
                 .replace(", +", "\n+")
                 .replace(", -", "\n-")
