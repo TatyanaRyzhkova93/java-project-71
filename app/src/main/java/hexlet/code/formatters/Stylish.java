83658d3cca5b3cd8086081c95414336ddf9fef51
Ryzhkova <Tatyana.Va.Ryzhkova@rosbank.ru>
2024-06-14 21:39:20 +0300

    yyyyy

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -9,12 +9,13 @@ public class Stylish implements Format {
         StringBuilder stb = new StringBuilder();
         stb.append("{\n");
         for (Map.Entry<String, Object> m : map.entrySet()) {
-            stb.append(" ")
+            stb.append("  ")
                     .append(m.getKey()).append(": ")
                     .append(m.getValue() == null ? null : m.getValue().toString())
                     .append("\n");
         }
         stb.append("}");
+       // System.out.println(stb.toString());
         return stb.toString();
     }
 }
